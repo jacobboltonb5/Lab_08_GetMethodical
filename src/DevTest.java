37200309cb0b97b10da607a66647e49af8a86427
jacob <boltonjb@mail.uc.edu>
2026-07-08 23:13:03 -0400

    update

--- src/DevTest.java
+++ src/DevTest.java
@@ -10,31 +10,31 @@ public class DevTest
         String name = SafeInput.getNonZeroLenString(in, "Enter your name");
         System.out.println("You entered: " + name);
 
-        // Part B test
-        // int anyInt = SafeInput.getInt(in, "Enter any integer");
-        // System.out.println("You entered: " + anyInt);
+//         Part B test
+         int anyInt = SafeInput.getInt(in, "Enter any integer");
+         System.out.println("You entered: " + anyInt);
 
-        // Part C test
-        // double anyDouble = SafeInput.getDouble(in, "Enter any double");
-        // System.out.println("You entered: " + anyDouble);
+//         Part C test
+         double anyDouble = SafeInput.getDouble(in, "Enter any double");
+         System.out.println("You entered: " + anyDouble);
+//
+//         Part D test
+         int rangedInt = SafeInput.getRangedInt(in, "Enter an integer", 1, 10);
+         System.out.println("You entered: " + rangedInt);
 
-        // Part D test
-        // int rangedInt = SafeInput.getRangedInt(in, "Enter an integer", 1, 10);
-        // System.out.println("You entered: " + rangedInt);
+//         Part E test
+         double rangedDouble = SafeInput.getRangedDouble(in, "Enter a double", 0.5, 10.0);
+         System.out.println("You entered: " + rangedDouble);
 
-        // Part E test
-        // double rangedDouble = SafeInput.getRangedDouble(in, "Enter a double", 0.5, 10.0);
-        // System.out.println("You entered: " + rangedDouble);
+//         Part F test
+         boolean yesNo = SafeInput.getYNConfirm(in, "Do you like pizza?");
+         System.out.println("Method returned: " + yesNo);
 
-        // Part F test
-        // boolean yesNo = SafeInput.getYNConfirm(in, "Do you like pizza?");
-        // System.out.println("Method returned: " + yesNo);
+//         Part G test
+         String ssn = SafeInput.getRegExString(in, "Enter your SSN (###-##-####)", "^\\d{3}-\\d{2}-\\d{4}$");
+         System.out.println("You entered: " + ssn);
 
-        // Part G test
-        // String ssn = SafeInput.getRegExString(in, "Enter your SSN (###-##-####)", "^\\d{3}-\\d{2}-\\d{4}$");
-        // System.out.println("You entered: " + ssn);
-
-        // Part H test (leave off - captured in Program 06)
-        // SafeInput.prettyHeader("Message Centered Here");
+//         Part H test
+         SafeInput.prettyHeader("Message Centered Here");
     }
 }
